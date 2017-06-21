ee3d38c49154bb695f8aa69bbb5f4d514fb27aac
Alden Prokup <rprokup@aldens-macbook-air.local>
2017-06-21 10:51:03 -0500

    Initial Commit

---
+++ Breakout ApplicationTests/Breakout_ApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Breakout_ApplicationTests.swift
+//  Breakout ApplicationTests
+//
+//  Created by Alden Prokup on 6/21/17.
+//  Copyright © 2017 Alden Prokup. All rights reserved.
+//
+
+import XCTest
+@testable import Breakout_Application
+
+class Breakout_ApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
